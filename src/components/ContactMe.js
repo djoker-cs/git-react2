a30a54dd006b351091cf966c2f1986d83d510ee6
Kevin <kevin_carl_laroza@dlsl.edu.ph>
2024-09-20 22:05:29 +0800

    commited everything

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -5,7 +5,7 @@ const ContactMe = () => {
     <div className="contact-me">
       <h2>Contact Me</h2>
       <p>School Email: <a href="kevin_carl_laroza@dlsl.edu.ph">kevin_carl_laroza@dlsl.edu.ph</a></p>
-      <p>Personal Email: <a href="kevinlaroza134@gmail.com" target="_blank" rel="noopener noreferrer">kevinlaroza134@gmail.com</a></p>
+      <p>Personal Email: <a href="kevinlaroza134@gmail.com" target="_blank" rel="noopener noreferrer"></a></p>
     </div>
   );
 }
